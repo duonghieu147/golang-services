b13bd7a065e60de20b8162c6d3ca833b552b631f
DuongHieu <duonghieu4299@gmail.com>
2024-01-11 22:24:47 +0700

    implement room

---
+++ client/app.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import useWebSocket from 'react-use-websocket';
+
+import './App.css';
+
+const WS_URL = 'ws://127.0.0.1:8080/ws';
+
+function App() {
+  useWebSocket(WS_URL, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
+    }
+  });
+
+  return (
+    <div>Hello WebSockets!</div>
+  );
+}
+
+export default App;
